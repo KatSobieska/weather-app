e73cc94280b171dfc2835296dd9d0b5ababc74dd
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-19 18:54:42 +0200

    Add handleSubmit and use it in form

--- src/components/PickCity/PickCity.js
+++ src/components/PickCity/PickCity.js
@@ -1,16 +1,26 @@
-import Button from '../Button/Button';
-import TextInput from '../TextInput/TextInput';
-import styles from './PickCity.module.scss';
+import Button from "../Button/Button";
+import TextInput from "../TextInput/TextInput";
+import styles from "./PickCity.module.scss";
 
-import { useState } from 'react';
+import { useState } from "react";
 
-const PickCity = () => {
-  const [city, setCity] = useState('');
+const PickCity = (props) => {
+  const [city, setCity] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.action(city);
+    setCity("");
+  };
 
   return (
-    <form className={styles.pickCityForm}> 
+    <form className={styles.pickCityForm} onSubmit={handleSubmit}>
       <label>
-        <TextInput placeholder="Enter city name...." value={city} onChange={e => setCity(e.target.value)} />
+        <TextInput
+          placeholder="Enter city name...."
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
       </label>
       <Button>Search</Button>
     </form>
