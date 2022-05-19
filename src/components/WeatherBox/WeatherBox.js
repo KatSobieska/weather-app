f0483b822d425740c54082b7c90e42d3273d4711
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-19 19:27:04 +0200

    Add fetch for handleCityChange

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -5,8 +5,15 @@ import { useCallback } from "react";
 
 const WeatherBox = () => {
   const handleCityChange = useCallback((city) => {
-    console.log(city);
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=93c3ef7ff071b6442b584c7cc7cb23e2&units=metric`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
   }, []);
+
   return (
     <section>
       <PickCity action={handleCityChange} />
