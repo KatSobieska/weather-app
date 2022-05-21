f95c7b168bfff4ef81cc62e9131913a05ae24e54
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-21 19:06:19 +0200

    Add error for not found city

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -11,18 +11,22 @@ const WeatherBox = () => {
     setPending(true);
     fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=93c3ef7ff071b6442b584c7cc7cb23e2&units=metric`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        const weatherData = {
-          city: data.name,
-          temp: data.main.temp,
-          icon: data.weather[0].icon,
-          description: data.weather[0].main,
-        };
-        setCityWeatherData(weatherData);
-        setPending(false);
-      });
+    ).then((res) => {
+      if (res.status === 200) {
+        return res.json().then((data) => {
+          const weatherData = {
+            city: data.name,
+            temp: data.main.temp,
+            icon: data.weather[0].icon,
+            description: data.weather[0].main,
+          };
+          setCityWeatherData(weatherData);
+          setPending(false);
+        });
+      } else {
+        alert("ERROR!");
+      }
+    });
   }, []);
 
   return (
